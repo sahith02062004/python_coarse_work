fcb8afab69fae7a967ddf5b513fed0886e3fa277
Sriramoju Sahith <sriramojusahith80@gmail.com>
2026-05-30 11:44:32 +0530

    Add files via upload

---
+++ phthon.py
@@ -0,0 +1,77 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+a=20
+b=10
+a+b
+30
+a-b
+10
+a*b
+200
+a/b
+2.0
+9/2
+4.5
+9//2
+4
+a**2
+400
+6**3
+216
+a%b
+0
+17%4
+1
+17%3
+2
+a%b
+0
+17%4
+1
+17%3
+2
+a
+20
+b
+10
+>>> a<b
+False
+>>> a>b
+True
+>>> a<=b
+False
+>>> 10<=b
+True
+>>> a>=b
+True
+>>> a==b
+False
+>>> a!=b
+True
+>>> y=5
+>>> y
+5
+>>> y=y+5
+>>> y
+10
+>>> y=y=10
+>>> y
+10
+>>> y=y+10
+>>> y
+20
+>>> y+=10
+>>> y
+30
+>>> y-=20
+>>> y
+10
+>>> y*=4
+>>> y
+40
+>>> y//=4
+>>> y
+10
+>>> y%=2
+>>> y
+0
